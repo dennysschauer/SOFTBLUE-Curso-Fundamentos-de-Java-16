9d02691986aace7d8e7e4f7d387538957f35fb5f
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-06-15 23:55:15 -0300

    Create Aula_3_4_castingTipoPrimitvo.java

---
+++ src/conceitosiniciais/Aula_3_4_castingTipoPrimitvo.java
@@ -0,0 +1,24 @@
+package conceitosiniciais;
+
+public class Aula_3_4_castingTipoPrimitvo {
+
+	public static void main(String[] args) {
+
+		//long x = 10;
+		
+		//double x = 35;
+		//System.out.println(x);
+		
+		//float x = 40.0F;
+		
+		//float x = (float) 40.0;
+		
+		//short x = (short) 100000;
+		//System.out.println(x);
+		
+		byte x = (byte) 130;
+		System.out.println(x);
+
+	}
+
+}
