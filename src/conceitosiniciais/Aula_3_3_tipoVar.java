cece15f6c2ac3565ebfba42d63270980917f7404
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-06-15 23:41:53 -0300

    Create Aula_3_3_tipoVar.java

---
+++ src/conceitosiniciais/Aula_3_3_tipoVar.java
@@ -0,0 +1,20 @@
+package conceitosiniciais;
+
+public class Aula_3_3_tipoVar {
+
+	public static void main(String[] args) {
+
+		int x = 10;
+		var x2 = 20;
+		x2 = 15;
+		
+		boolean y = false;
+		var y2 = true;
+		
+		//var z;
+		
+		System.out.println(x2);
+
+	}
+
+}
