e948ba78b733a3e3643f22df4b82d3de7ada59da
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-06-16 23:35:47 -0300

    Create Aula_3_7_while_doWhile_for.java

---
+++ src/conceitosiniciais/Aula_3_7_while_doWhile_for.java
@@ -0,0 +1,74 @@
+package conceitosiniciais;
+
+public class Aula_3_7_while_doWhile_for {
+
+	public static void main(String[] args) {
+		
+//		int x = 0;
+//		
+//		while (x <= 10) {
+//			System.out.println(x);
+//			x++;
+//		}
+		
+//		int x = 0;
+//		
+//		while (true) {
+//			
+//			System.out.println(x);
+//			
+//			if (x == 10) {
+//				break;
+//			}
+//			
+//			x++;
+//		}
+
+//		int x = 0;
+//		
+//		while (x < 10) {
+//			
+//			x++;
+//			
+//			if (x == 5) {
+//				continue;
+//			}
+//			
+//			System.out.println(x);
+//		}
+		
+//		int x = 0;
+//		
+//		do {
+//			System.out.println(x);
+//			x++;
+//			
+//		} while (x <= 10);
+		
+//		int i;
+//		int j;
+//		
+//		for (i = 0, j = 10; i < 10; i++, j--) {
+//			System.out.println(i + " - " + j);
+//		}
+		
+//		int i = 0;
+//		
+//		for(; i < 10;) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+		int i = 0;
+		
+		for ( ; ; ) {
+			System.out.println(i);
+			i++;
+			if(i == 10) {
+				break;
+			}
+		}
+		
+	}
+
+}
