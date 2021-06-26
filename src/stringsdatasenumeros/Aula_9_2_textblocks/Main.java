432a752a2c1481623a93ed98a05d6dbc58a1af80
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-06-26 18:57:15 -0300

    text blocks

---
+++ src/stringsdatasenumeros/Aula_9_2_textblocks/Main.java
@@ -0,0 +1,26 @@
+package stringsdatasenumeros.Aula_9_2_textblocks;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		String html =
+//				"<html>\n" +
+//				"     <body>\n" +
+//				"          <h1>Texto Qualquer</h1>\n" +
+//				"     </body>\n" +
+//				"</html>";
+
+		String html = 
+				"""
+				<html>
+				   <body>
+					  <h1>Texto Qualquer</h1>
+				   </body>
+				</html>
+				""";
+
+		System.out.println(html);
+	}
+
+}
