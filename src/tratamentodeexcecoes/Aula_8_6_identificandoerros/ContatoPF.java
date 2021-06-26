1ac94f4134d54f1e04d1971b768dd2ef3d5d2da1
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-06-25 21:08:12 -0300

    Identificando erros em aplicações Java

---
+++ src/tratamentodeexcecoes/Aula_8_6_identificandoerros/ContatoPF.java
@@ -0,0 +1,13 @@
+package tratamentodeexcecoes.Aula_8_6_identificandoerros;
+
+public class ContatoPF extends Contato {
+	private String cpf;
+	
+	public String cpf() {
+		return cpf;
+	}
+	
+	public void setCPF(String cpf) {
+		this.cpf = cpf;
+	}
+}
---
+++ src/tratamentodeexcecoes/Aula_8_6_identificandoerros/ContatoPJ.java
@@ -0,0 +1,13 @@
+package tratamentodeexcecoes.Aula_8_6_identificandoerros;
+
+public class ContatoPJ extends Contato {
+	private String cnpj;
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+	public void setCNPJ(String cnpj) {
+		this.cnpj = cnpj;
+	}
+}
