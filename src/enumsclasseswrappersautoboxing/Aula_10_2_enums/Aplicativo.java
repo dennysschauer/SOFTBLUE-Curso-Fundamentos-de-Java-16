7d6fe9d80f56ec3e1c494ac6f1e98ff8c74539a2
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-06-27 17:49:18 -0300

    Criando enums

---
+++ src/enumsclasseswrappersautoboxing/Aula_10_2_enums/Aplicativo.java
@@ -0,0 +1,24 @@
+package enumsclasseswrappersautoboxing.Aula_10_2_enums;
+
+public class Aplicativo {
+		
+	private enum Sexo{
+		MASCULINO, FEMININO
+	}
+	
+	public static void main(String[] args) {
+		
+		DiaDaSemana hoje = DiaDaSemana.SEXTA;
+		
+		int num = DiaDaSemana.QUARTA.getNum();
+		System.out.println(num);
+		
+		String s = "TERCA";
+		
+		DiaDaSemana d = DiaDaSemana.valueOf(s);
+		System.out.println(d.getNum());
+		
+		Sexo sexo = Sexo.MASCULINO;
+	}
+
+}
