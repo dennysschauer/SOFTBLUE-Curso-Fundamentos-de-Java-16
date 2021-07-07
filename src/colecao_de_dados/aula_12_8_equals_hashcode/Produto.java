09793bd9e27bf42c5796419c1fc3f1994a3b04e4
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-07-06 21:49:52 -0300

    métodos equals() e hashCode()

---
+++ src/colecao_de_dados/aula_12_8_equals_hashcode/Produto.java
@@ -0,0 +1,73 @@
+package colecao_de_dados.aula_12_8_equals_hashcode;
+
+public class Produto {
+	private int id;
+	private String descricao;
+	
+	public Produto(int id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return id + "," + descricao;
+	}
+
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + id;
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		Produto other = (Produto) obj;
+//		if (id != other.id)
+//			return false;
+//		return true;
+//	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+
+		if (!(obj instanceof Produto)) {
+			return false;
+		}
+
+		Produto other = (Produto) obj;
+
+		if (this.id == other.id) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		//return (int) (Math.random() * 10);
+		return String.valueOf(id).hashCode();
+	}
+
+	
+}
