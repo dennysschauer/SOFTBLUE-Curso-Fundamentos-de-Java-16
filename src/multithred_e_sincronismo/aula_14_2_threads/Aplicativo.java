c02bdb65c8a95de111fa400493d3c160db3b70a3
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-07-08 22:50:55 -0300

    executando threads

---
+++ src/multithred_e_sincronismo/aula_14_2_threads/Aplicativo.java
@@ -0,0 +1,24 @@
+package multithred_e_sincronismo.aula_14_2_threads;
+
+public class Aplicativo {
+
+	public static void main(String[] args) {
+		//Contador c = new Contador();
+		//c.start();
+		
+		Contador2 c = new Contador2();
+		Thread t = new Thread(c);
+		t.start();
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ src/multithred_e_sincronismo/aula_14_2_threads/Contador2.java
@@ -0,0 +1,19 @@
+package multithred_e_sincronismo.aula_14_2_threads;
+
+public class Contador2 implements Runnable {
+
+	@Override
+	public void run() {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("=>" + i);
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
