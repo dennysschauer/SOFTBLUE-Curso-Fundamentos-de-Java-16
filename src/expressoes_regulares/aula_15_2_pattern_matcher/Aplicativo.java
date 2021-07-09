eb08b9acf74d5e6569567fb811540e2f045ffaba
dennysschauer <82009101+dennysschauer@users.noreply.github.com>
2021-07-09 09:04:11 -0300

    Pattern, Matcher e tokenizacao

---
+++ src/expressoes_regulares/aula_15_2_pattern_matcher/Aplicativo.java
@@ -0,0 +1,40 @@
+package expressoes_regulares.aula_15_2_pattern_matcher;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Aplicativo {
+
+	public static void main(String[] args) {
+//		Pattern p = Pattern.compile("([a-z])+");
+//		Matcher m = p.matcher("a1bc2def3");
+//		
+//		while (m.find()) {
+//			int pos = m.start();
+//			String group = m.group();
+//			
+//			System.out.println(pos + " => " + group);
+//		}
+		
+		Pattern p = Pattern.compile("[a-z]");
+		Matcher m = p.matcher("a1bc2def3");
+		
+		StringBuffer sb = new StringBuffer();
+		
+		while (m.find()) {
+			int pos = m.start();
+			String group = m.group();
+			
+			group = "?";
+			m.appendReplacement(sb, group);
+			
+			System.out.println(pos + " => " + group);
+		}
+		
+		m.appendTail(sb);
+		
+		System.out.println(sb);
+
+	}
+
+}
